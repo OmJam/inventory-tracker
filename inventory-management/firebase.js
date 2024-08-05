9c0226872782763678723e11cb291c3a9482c0d4
OmJam <101439736+OmJam@users.noreply.github.com>
2024-08-04 23:04:11 -0700

    fixed api call

--- inventory-management/firebase.js
+++ inventory-management/firebase.js
@@ -1,14 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+require("dotenv").config({ path: "inventory.env" });
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const apiKeyfb = process.env.API_KEY;
+
 const firebaseConfig = {
-  apiKey: "AIzaSyD6up3i9xZebGjA0knhRKdUW_bZjdzpmUw",
+  apiKey: apiKeyfb,
   authDomain: "inventory-managementfb.firebaseapp.com",
   projectId: "inventory-managementfb",
   storageBucket: "inventory-managementfb.appspot.com",
